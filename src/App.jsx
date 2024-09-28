f43c103b9e6290c6c7d10e6cff3c70eeb172be86
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-28 19:03:28 +0530

    components of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { useDispatch, useState } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import { login, logout } from "./store/authSLice";
 
 function App() {
-  console.log(import.meta.env.VITE_SOME_KEY); // "123"
-  console.log(import.meta.env.DB_PASSWORD); // undefined
-  return (
-    <>
-      <h1>mimohshukla</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {});
+  }, []);
+
+  return <></>;
 }
 
 export default App;
